642590d30d8c55fd233dcc39544e120009ab473f
Eason <eason@example.com>
2026-01-29 18:59:06 -0800

    Deploy 01/29/2026 Thu 18:59:06.60: Mini App deployment with version increment to v1.1.0249

--- src/App.tsx
+++ src/App.tsx
@@ -363,7 +363,7 @@ function App() {
               color: 'var(--tg-theme-hint-color)',
               marginTop: '2px'
             }}>
-              v1.1.0247</span>
+              v1.1.0249</span>
           </div>
           
           <div style={{ 
